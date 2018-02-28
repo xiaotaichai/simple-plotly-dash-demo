9b376f6a4f0a49a1c77960772255e2db63d1d6a3
Richard Decal <richard.decal@ncf.edu>
2018-02-28 13:01:28 -0500

    adding comments, deleting useless lines

--- app.py
+++ app.py
@@ -7,17 +7,17 @@ from datetime import datetime as dt
 import flask
 import os
 
+# We start a Flask server
 server = flask.Flask('app')
 server.secret_key = os.environ.get('secret_key', 'secret')
 
+# Dash then creates the app on the server
 app = dash.Dash('app', server=server)
 
-app.scripts.config.serve_locally = False
-dcc._js_dist[0]['external_url'] = 'https://cdn.plot.ly/plotly-basic-latest.min.js'
-
+# This is a Python wrapper for generating HTML code.
 app.layout = html.Div([
-    html.H1('Stock Tickers'),
-    dcc.Dropdown(
+    html.H1('Stock Tickers'),  # make a big header at the top
+    dcc.Dropdown(  # dash-core-components has a Dropdown template
         id='my-dropdown',
         options=[
             {'label': 'Coke', 'value': 'COKE'},
@@ -26,7 +26,7 @@ app.layout = html.Div([
         ],
         value='COKE'
     ),
-    dcc.Graph(id='my-graph')
+    dcc.Graph(id='my-graph')  # ... as well as a graph template
 ], className="container")
 
 @app.callback(Output('my-graph', 'figure'), [Input('my-dropdown', 'value')])
