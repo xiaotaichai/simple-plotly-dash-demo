a3c900df3dfd2ee08103cd4e306fa614af4a6e34
Richard Decal <richard.decal@ncf.edu>
2018-02-28 16:08:05 -0500

    added slider and chipotle, new title

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 **Dash Hello World**
 
+This app fetches finance data from Google and makes a simple line graph. It lets you pick a ticker from a dropdown.
+
 This app is a simple example of the capabilities of Dash developed by [Plotly](https://plot.ly/) and acts as an intro to overall Python framework. Further examples can be found at [dash/gallery/](plot.ly/dash/gallery).
 
 Dash abstracts away all of the technologies and protocols required to build an interactive web-based application and is a simple and effective way to bind a user interface around your Python code.
@@ -9,4 +11,3 @@ To learn more check out our [documentation](https://plot.ly/dash).
 The following are screenshots for the app in this repo:
 ![Alt desc](https://github.com/plotly/dash-hello-world/raw/master/Screenshots/Dash-Hello-World-Photo.png)
 
-This app 
--- app.py
+++ app.py
@@ -16,28 +16,36 @@ app = dash.Dash('app', server=server)
 
 # This is a Python wrapper for generating HTML code.
 app.layout = html.Div([
-    html.H1('Stock Tickers'),  # make a big header at the top
+    html.H1('My better stock ticker title'),  # make a big header at the top
     dcc.Dropdown(  # dash-core-components has a Dropdown template
         id='my-dropdown',
         options=[
             {'label': 'Coke', 'value': 'COKE'},
             {'label': 'Tesla', 'value': 'TSLA'},
+            {'label': 'Chipotle', 'value': 'CPE'},
             {'label': 'Apple', 'value': 'AAPL'}
         ],
         value='COKE'  # the default value when you open the app
     ),
-    dcc.Graph(id='my-graph')  # ... as well as a graph template that takes a
+    dcc.Graph(id='my-graph'),  # ... as well as a graph template that takes a
                               # dictionary created by update_graph()
+    dcc.DatePickerRange(
+        id='date-picker-range',
+        start_date=dt(2001, 1, 1),
+        end_date=dt.now()
+    )
 ], className="container")
 
 # this is a Python decorator. It feeds update_graph() into app.callback().
 # It fetches the value from my-dropdown and feeds it into update_graph() as
 # selected_dropdown_value.
-@app.callback(Output('my-graph', 'figure'), [Input('my-dropdown', 'value')])
-def update_graph(selected_dropdown_value):
+@app.callback(Output('my-graph', 'figure'), [Input('my-dropdown', 'value'),
+                                             Input('date-picker-range', 'start_date'),
+                                             Input('date-picker-range', 'end_date')])
+def update_graph(selected_dropdown_value, start_date, end_date):
     df = web.DataReader(
         selected_dropdown_value, data_source='google',
-        start=dt(2017, 1, 1), end=dt.now())
+        start=start_date, end=end_date)
 
     # return a dictionary which is fed to my-graph
     return {
